fe6fad20b41888bd8ca7abdda6f716fae0b1166b
JonatasDataEng <jonatas.dataeng@gmail.com>
2024-09-03 02:55:59 -0300

    Delete dags directory

--- dags/modules/__init__.py
+++
@@ -1,7 +0,0 @@
-from .utils import get_defaultairflow_args
-from .extract import extract_data_from_api
-from .config_api import config_api
-from .DataConn import DataConn
-from .load import load_data
-from .transform import pre_processing_data
-from .utils import get_defaultairflow_args
